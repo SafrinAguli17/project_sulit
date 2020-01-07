794ebf195d86bfe1c0ae52d99dc9f1ddf71b1188
yogipratama1 <pyogi6391@gmail.com>
2020-01-07 13:08:48 +0700

    update set penguji,hasil ujian

---
+++ application/views/Mahasiswa/v_hasilujian.php
@@ -0,0 +1,54 @@
+<div class="right_col" role="main" style="min-height: 1164px;>
+       <br />
+       <div class="row">
+          <div class="col-md-12 col-sm-4 ">
+            <div class="x_panel">
+              <div class="x_title">
+              <h2>HASIL UJIAN SKRIPSI</h2>
+              <ul class="nav navbar-right panel_toolbox">
+                  <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                  </li>
+                  <li class="dropdown">
+                    <a href="#" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                  </li>
+                  <li><a class="close-link"><i class="fa fa-close"></i></a>
+                  </li>
+                </ul>
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_content">
+                <div class="dashboard-widget-content">
+      <table stytle="margin=20px auto;" border="1" >
+      <tr>
+          <td>No</td>
+          <td>Nama Mahasiswa</td>
+          <td>Nilai</td>
+      </tr>
+      <tr>
+          <td>1</td>
+          <td>Mr. X</td>
+          <td> . . . </td>
+          </tr>
+      <tr>
+          <td>2</td>
+          <td>Mr. Y</td>
+          <td> . . . </td>
+      </tr>
+      <tr>
+          <td>3</td>
+          <td>Mr. Z</td>
+          <td> . . . </td>
+      </tr>
+
+
+      </table>
+      <br/>
+      <input style="border-radius: 4px; border: none; height: 23px; color: white; background: #2A3F54;" 
+      type="button" value="PRINT">
+      </div>
+      </div>
+          </div>
+            </div>
+              </div>
+            </div>
+          </div>
